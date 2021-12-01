fa0fb3380fddffc2ee1f465268add40416894c2b
Russell Gordon <rgordon@lcs.on.ca>
2021-12-01 10:22:49 -0500

    Changed the purely static layout so that it uses the stored property and computed property instead and shows their values.

--- FiguresHelper/Views/CircleView.swift
+++ FiguresHelper/Views/CircleView.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 struct CircleView: View {
     
+    // MARK: Stored properties
+    // "radius" is the name
+    // "Double" is the data type
+    // By adding = 15.00    we are just providing a default value.
+    var radius: Double = 45.00
+    
+    // MARK: Computed properties
+    var area: Double {
+        return Double.pi * radius * radius
+    }
+    
     // User interface
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
@@ -22,7 +33,7 @@ struct CircleView: View {
                 // Show the selected radius value
                 HStack {
                     Spacer()
-                    Text("\( String(format: "%.2f", 15.0) )")
+                    Text("\(radius)")
                         .font(.title2)
                         .bold()
                     Spacer()
@@ -47,7 +58,7 @@ struct CircleView: View {
             Text("Area:")
                 .bold()
             
-            Text("706.9 square units")
+            Text("\(area) square units")
                 .font(.title2)
             
             
